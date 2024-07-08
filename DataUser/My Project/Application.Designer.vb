4ae19a56c95391ce89631df7653c9bb59469a13a
Celrik08 <m.fajar080907@gmail.com>
2024-07-08 13:24:11 +0800

    Commit2

--- DataUser/My Project/Application.Designer.vb
+++ DataUser/My Project/Application.Designer.vb
@@ -31,7 +31,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = DataPegawai
+            Me.MainForm = DataSiswa
         End Sub
     End Class
 End Namespace
